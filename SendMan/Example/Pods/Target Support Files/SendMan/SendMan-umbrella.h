c4618d72a0b225dd1ac93d9e66e4b1a8df113bfc
Anat Harari <harari.anat@gmail.com>
2020-03-22 16:52:08 +0200

    Fix Models and JSONModel path

--- SendMan/Example/Pods/Target Support Files/SendMan/SendMan-umbrella.h
+++ SendMan/Example/Pods/Target Support Files/SendMan/SendMan-umbrella.h
@@ -10,8 +10,22 @@
 #endif
 #endif
 
+#import "JSONModel.h"
+#import "JSONModelClassProperty.h"
+#import "JSONModelError.h"
+#import "JSONModelLib.h"
+#import "JSONKeyMapper.h"
+#import "JSONValueTransformer.h"
+#import "SMCustomEvent.h"
+#import "SMData.h"
+#import "SMPropertyValue.h"
+#import "SSMDKEvent.h"
+#import "SSMession.h"
+#import "SMAPIHandler.h"
+#import "SMAuthHandler.h"
 #import "SMConfig.h"
 #import "SMDataCollector.h"
+#import "SMDataEnricher.h"
 
 FOUNDATION_EXPORT double SendManVersionNumber;
 FOUNDATION_EXPORT const unsigned char SendManVersionString[];
