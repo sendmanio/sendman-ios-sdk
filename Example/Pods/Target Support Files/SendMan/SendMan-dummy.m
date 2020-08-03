fbae86651e5658f2a5d78994455d9171ebb78456
Anat Harari <harari.anat@gmail.com>
2020-08-03 13:02:41 +0300

    Change most pod files to private - partial solution

--- Example/Pods/Target Support Files/SendMan/SendMan-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SendMan : NSObject
-@end
-@implementation PodsDummy_SendMan
-@end
--- Example/Pods/Target Support Files/SendMan/SendMan-umbrella.h
+++ Example/Pods/Target Support Files/SendMan/SendMan-umbrella.h
@@ -1,41 +1,4 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#else
-#ifndef FOUNDATION_EXPORT
-#if defined(__cplusplus)
-#define FOUNDATION_EXPORT extern "C"
-#else
-#define FOUNDATION_EXPORT extern
-#endif
-#endif
-#endif
-
-#import "JSONModel.h"
-#import "JSONModelClassProperty.h"
-#import "JSONModelError.h"
-#import "JSONModelLib.h"
-#import "JSONKeyMapper.h"
-#import "JSONValueTransformer.h"
-#import "SMCustomEvent.h"
-#import "SMData.h"
-#import "SMPropertyValue.h"
-#import "SMSDKEvent.h"
-#import "SMSession.h"
 #import "SendMan.h"
-#import "SMAPIHandler.h"
-#import "SMAuthHandler.h"
-#import "SMCategoriesHandler.h"
 #import "SMConfig.h"
-#import "SMDataCollector.h"
-#import "SMDataEnricher.h"
-#import "SMLifecycleHandler.h"
-#import "SMUtils.h"
-#import "SMNotificationCellDelegate.h"
-#import "SMNotificationsFooterCell.h"
-#import "SMNotificationsHeaderCell.h"
 #import "SMNotificationsViewController.h"
-#import "SMNotificationTableViewCell.h"
-
-FOUNDATION_EXPORT double SendManVersionNumber;
-FOUNDATION_EXPORT const unsigned char SendManVersionString[];
 
