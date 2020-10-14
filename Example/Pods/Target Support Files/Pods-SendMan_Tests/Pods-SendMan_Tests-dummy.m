f523b426f36f8b233d08eacf6e363c466171ac6f
anatsh <harari.anat@gmail.com>
2020-10-14 17:12:30 +0300

    Upgarde project to xcode 12 recommended settings

--- Example/Pods/Target Support Files/Pods-SendMan_Tests/Pods-SendMan_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SendMan_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_SendMan_Tests
-@end
--- Example/Pods/Target Support Files/SendMan/SendMan-umbrella.h
+++ Example/Pods/Target Support Files/SendMan/SendMan-umbrella.h
@@ -1,4 +1,42 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "JSONModel.h"
+#import "JSONModelClassProperty.h"
+#import "JSONModelError.h"
+#import "JSONKeyMapper.h"
+#import "JSONValueTransformer.h"
+#import "SMCategory.h"
+#import "SMData.h"
+#import "SMPropertyValue.h"
+#import "SMSDKEvent.h"
+#import "SMSession.h"
 #import "SendMan.h"
+#import "SMAPIHandler.h"
+#import "SMAuthHandler.h"
+#import "SMCategoriesHandler.h"
 #import "SMConfig.h"
+#import "SMDataCollector.h"
+#import "SMDataEnricher.h"
+#import "SMLifecycleHandler.h"
+#import "SMLog.h"
+#import "SMSessionManager.h"
+#import "SMUtils.h"
+#import "SMNotificationCellDelegate.h"
+#import "SMNotificationsFooterCell.h"
+#import "SMNotificationsHeaderCell.h"
 #import "SMNotificationsViewController.h"
+#import "SMNotificationTableViewCell.h"
+
+FOUNDATION_EXPORT double SendManVersionNumber;
+FOUNDATION_EXPORT const unsigned char SendManVersionString[];
 
